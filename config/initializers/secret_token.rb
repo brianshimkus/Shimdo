0b64d70d45cedecf7e4ed5c70c3b603ca02052f7
Brian <BrianShimkus1@gmail.com>
2014-02-03 16:25:28 -0600

    Initial commit of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shimdo::Application.config.secret_key_base = '86d3670893f2c449ab96d61e30ba7ffff9553d2335a9534f21d40d10e37780d7c79dcfb643da280c504673f0850bad58298d25261da7612435b8a1f03d2e277b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shimdo::Application.config.session_store :cookie_store, key: '_shimdo_session'
