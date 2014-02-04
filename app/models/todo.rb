fe42b2eb1f21e528903b32ffc6d361e82a61aa12
Brian <BrianShimkus1@gmail.com>
2014-02-03 19:30:20 -0600

    Initialized edit spec

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ActiveRecord::Base
-	validates :title, presence: true,
+	validates :title, presence: true
 	validates :title, length: { minimum: 3 }
 	validates :description, presence: true
 	validates :description, length: { minimum: 5 }
---
+++ spec/features/todo/edit_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "Editing todo" do
+	it "updates a todo list successfully with correct information" do
+		visit "/todo"
+	end
+end
