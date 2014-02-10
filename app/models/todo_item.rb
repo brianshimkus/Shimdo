cd9b61f26235cf728831fecb4cb277c88d0384ae
Brian <BrianShimkus1@gmail.com>
2014-02-10 01:53:13 -0600

    Show completed todo status

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,6 +4,11 @@ class TodoItem < ActiveRecord::Base
   validates :content, presence: true,
   					  length: { minimum: 2 }
 
+ 	class TodoItem < ActiveRecord::Base
+  	scope :complete, -> { where("completed_at is not null") }
+  	scope :incomplete, -> { where(completed_at: nil) }
+	end
+
   def completed?
   	!completed_at.blank?
   end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 
 describe Todo do
   it { should have_many(:todo_items) }
+
+  describe "#has_complete_items?" do
+  	let(:todo_list) { Todo.create(title: "Groceries", description: "Grocery list")}
+  	
+  	it "returns true with completed todo list items" do
+  		todo.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo.has_complete_items?).to be_true
+  	end
+
+  	it "returns flase with no completed todo list items" do
+  		todo.todo_items.create(content: "Eggs")
+  		expect(todo.has_complete_items?).to be_false
+  	end
+  end
+
+  describe "#has_incomplete_items?" do
+  	let(:todo_list) { Todo.create(title: "Groceries", description: "Grocery list")}
+  	
+  	it "returns true with incompleted todo list items" do
+  		todo.todo_items.create(content: "Eggs")
+  		expect(todo.has_incomplete_items?).to be_true
+  	end
+
+  	it "returns flase with no incomplete todo list items" do
+  		todo.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo.has_incomplete_items?).to be_false
+  	end
+  end
+
 end
