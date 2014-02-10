4ef42787972e131ec4409faa6e22b07ef9a2c3e3
Brian <BrianShimkus1@gmail.com>
2014-02-10 00:29:21 -0600

    Cleaned up todo mark completion view

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -2,5 +2,18 @@ require 'spec_helper'
 
 describe TodoItem do
 	it { should belong_to(:todo) }
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	describe "#completed?" do
+		let(:todo_item) { TodoItem.create(content: "Hello") }
+
+		it "is false when completed_is blank" do 
+			todo_item.completed_at = nil
+			expect(todo_item.completed?).to be_false
+		end
+
+		it "returns true when completed_at is not empty" do
+			todo_item.completed_at = Time.now
+			expect(todo_item.completed?).to be_true
+		end
+	end
 end
